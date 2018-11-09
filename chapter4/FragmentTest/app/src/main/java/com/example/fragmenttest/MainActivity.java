53ea1bbc9ddc26cbf436b8947d3fdde5bf5ddbf7
syYang <yangsyun@gmail.com>
2018-11-09 10:10:06 +0800

    修订：第四章的碎片管理

--- chapter4/FragmentTest/app/src/main/java/com/example/fragmenttest/MainActivity.java
+++ chapter4/FragmentTest/app/src/main/java/com/example/fragmenttest/MainActivity.java
@@ -16,26 +16,26 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         Button button = (Button) findViewById(R.id.button);
         button.setOnClickListener(this);
-//        replaceFragment(new RightFragment());
+      replaceFragment(new RightFragment());
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.button:
-//                replaceFragment(new AnotherRightFragment());
+               replaceFragment(new AnotherRightFragment());
                 break;
             default:
                 break;
         }
     }
 
-//    private void replaceFragment(Fragment fragment) {
-//        FragmentManager fragmentManager = getSupportFragmentManager();
-//        FragmentTransaction transaction = fragmentManager.beginTransaction();
-//        transaction.replace(R.id.right_layout, fragment);
-//        transaction.addToBackStack(null);
-//        transaction.commit();
-//    }
+   private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.right_layout, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+   }
 
 }
