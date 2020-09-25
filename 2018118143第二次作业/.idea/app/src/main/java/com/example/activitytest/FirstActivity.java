b0c57446b3459dc50c833b6f666a4a1cba7057b1
irrigate-brain <70872221+irrigate-brain@users.noreply.github.com>
2020-09-25 23:24:10 +0800

    更新1

---
+++ 2018118143第二次作业/.idea/app/src/main/java/com/example/activitytest/FirstActivity.java
@@ -0,0 +1,65 @@
+package com.example.activitytest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class FirstActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d("FirstActivity",this.toString());
+        setContentView(R.layout.first_layout);
+        Button button1 = (Button) findViewById(R.id.button_1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(FirstActivity.this,SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main,menu);
+        return true;
+    }
+
+    public boolean OnOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+        case R.id.add_item:
+             Toast.makeText(this,"You clicked Add",Toast.LENGTH_SHORT).show();
+             break;
+        case R.id.remove_item:
+            Toast.makeText(this,"You clicked Remove",Toast.LENGTH_SHORT).show();
+            break;
+        default:
+        }
+        return true;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case 1:
+                if (resultCode == RESULT_OK) {
+                    String returnedData = data.getStringExtra("data_return");
+                    Log.d("FirstActivity", returnedData);
+                }
+                break;
+            default:
+        }
+    }
+
+
+}
