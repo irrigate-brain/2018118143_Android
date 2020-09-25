105a56eb9fb586381aa4fe0d442f1b1bbbca343f
irrigate-brain <70872221+irrigate-brain@users.noreply.github.com>
2020-09-25 13:10:43 +0800

    第二次作业

---
+++ 2018118143第二次作业/app/src/main/java/com/example/activitytest/FirstActivity.java
@@ -0,0 +1,45 @@
+package com.example.activitytest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class FirstActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.first_layout);
+        Button button1 = (Button) findViewById(R.id.button_1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(FirstActivity.this,"You clicked Button 1",Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main,menu);
+        return true;
+    }
+
+    public boolean OnOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+        case R.id.add_item:
+             Toast.makeText(this,"You clicked Add",Toast.LENGTH_SHORT).show();
+             break;
+        case R.id.remove_item:
+            Toast.makeText(this,"You clicked Remove",Toast.LENGTH_SHORT).show();
+            break;
+        default:
+        }
+        return true;
+    }
+}
