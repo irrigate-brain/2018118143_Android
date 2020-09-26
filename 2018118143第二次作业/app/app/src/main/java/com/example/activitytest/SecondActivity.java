894dea70a792ae22b9bba161668d04d0462d9779
irrigate-brain <70872221+irrigate-brain@users.noreply.github.com>
2020-09-27 01:43:41 +0800

    更新3

---
+++ 2018118143第二次作业/app/app/src/main/java/com/example/activitytest/SecondActivity.java
@@ -0,0 +1,45 @@
+package com.example.activitytest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d("SecondActivity",this.toString());
+        setContentView(R.layout.second_layout);
+        Button button2 = (Button) findViewById(R.id.button_2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SecondActivity.this,FirstActivity.class);
+                startActivity(intent);
+            }
+        });
+        //Intent intent = getIntent();
+        //String data = intent.getStringExtra("extra_data");
+        //Log.d("SecondActivity",data);
+    }
+
+    @Override
+    public void onBackPressed() {
+        Intent intent = new Intent();
+        intent.putExtra("data_return", "Hello FirstActivity");
+        setResult(RESULT_OK, intent);
+        finish();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("SecondActivity","onDestroy");
+    }
+
+}
