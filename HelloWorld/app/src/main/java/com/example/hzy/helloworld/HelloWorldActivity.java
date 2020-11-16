54b68e9dcd479eaadad496e18622714e638a5ecf
irrigate-brain <70872221+irrigate-brain@users.noreply.github.com>
2020-11-16 16:11:43 +0800

    更新1

---
+++ HelloWorld/app/src/main/java/com/example/hzy/helloworld/HelloWorldActivity.java
@@ -0,0 +1,13 @@
+package com.example.hzy.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle; 
+
+public class HelloWorldActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.hello_world_layout);
+    }
+}
