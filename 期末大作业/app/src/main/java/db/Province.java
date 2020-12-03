1d5a6ca08d7cd3ea2c7ec988b013c1b772021f39
irrigate-brain <70872221+irrigate-brain@users.noreply.github.com>
2020-12-03 18:13:47 +0800

    更新1

---
+++ 期末大作业/app/src/main/java/db/Province.java
@@ -0,0 +1,34 @@
+package db;
+
+public class Province extends DataSupport {
+
+    private int id;
+
+    private String provinceName;
+
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
