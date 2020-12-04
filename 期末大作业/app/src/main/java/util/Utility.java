d3b937232d50e13d0e84adeaa72919dc90ab031d
irrigate-brain <70872221+irrigate-brain@users.noreply.github.com>
2020-12-04 19:09:35 +0800

    更新6

--- 期末大作业/app/src/main/java/util/Utility.java
+++ 期末大作业/app/src/main/java/util/Utility.java
@@ -2,6 +2,8 @@ package util;
 
 import android.text.TextUtils;
 
+import com.google.gson.Gson;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -9,6 +11,7 @@ import org.json.JSONObject;
 import db.City;
 import db.County;
 import db.Province;
+import gson.Weather;
 
 public class Utility {
 
@@ -79,4 +82,20 @@ public class Utility {
         }
         return false;
     }
+
+    /**
+     * 将返回的JSON数据解析成Weather实体类
+     */
+    public static Weather handleWeatherResponse(String response) {
+        try {
+            JSONObject jsonObject = new JSONObject(response);
+            JSONArray jsonArray = jsonObject.getJSONArray("HeWeather");
+            String weatherContent = jsonArray.getJSONObject(0).toString();
+            return new Gson().fromJson(weatherContent, Weather.class);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
 }
